e7b12235555ce58ae40426e9054ad125fde5c001
Imani Manyara <hello@imanimanyara.com>
2021-03-04 20:01:52 -0500

    Some refactors

--- helpers/general/uuid.php
+++ helpers/general/uuid.php
@@ -1,2 +1,15 @@
 <?php
 
+use GpsLab\Component\Base64UID\Base64UID;
+
+if (!function_exists('base64Uid')) {
+    /**
+     * @param int $length
+     * @param string $charset
+     * @return mixed
+     */
+    function base64Uid($length = 12, $charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'){
+        return (new RandomCharGenerator($length, $charset))->generate();
+    }
+}
+
