a5b893d0401380f1bb669210525cce8956380727
Imani Manyara <hello@imanimanyara.com>
2021-03-08 09:35:15 -0500

    Some refactors

--- src/Facades/TypeConverterFacade.php
+++ src/Facades/TypeConverterFacade.php
@@ -2,17 +2,17 @@
 
 namespace Simtabi\Laranail\Facades;
 
+use Simtabi\Pheg\Phegs\Libraries\DataTools\TypeConverter;
 use Illuminate\Support\Facades\Facade;
 use Simtabi\Laranail\Supports\Languages;
-use Simtabi\Pheg\Phegs\Helpers\Components\DataTools\DataTypeConverter;
 
-class DataTypeConverterFacade extends Facade
+class TypeConverterFacade extends Facade
 {
     /**
      * @return string
      */
     protected static function getFacadeAccessor()
     {
-        return DataTypeConverter::class;
+        return TypeConverter::class;
     }
 }
